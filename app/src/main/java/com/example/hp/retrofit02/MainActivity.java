3c40f6acd0cc87426f29c111fdc9b4adfd1f1c63
Yurii Kotov <ykot@ciklum.com>
2016-05-30 16:15:45 +0300

    Resolved

--- app/src/main/java/com/example/hp/retrofit02/MainActivity.java
+++ app/src/main/java/com/example/hp/retrofit02/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.hp.retrofit02;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,10 +11,14 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.util.List;
 
-import retrofit2.*;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,45 +26,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TwitterService twitterService =  TwitterService.retrofit.create(TwitterService.class);
-                 Call<List<Contributor>> call = twitterService.repoContributors();
-                new NetworkCall().execute(call);
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: ");
+
+                    HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                    interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+                    OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://api.github.com/")
+                            .client(client)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+
+                    Call<List<Contributor>> call = retrofit.create(TwitterService.class).repoContributors();
+                    new NetworkCall().execute(call);
+                }
+            });
+        }
     }
-
-
 
     private class NetworkCall extends AsyncTask<Call, Void, String> {
 
         @Override
         protected String doInBackground(Call... params) {
-            try{
-                params[0].execute().body().toString();
-            }catch (IOException e){
+            Log.d(TAG, "onClick: ");
+            String response = "This is will be response";
+            try {
+                response = params[0].execute().body().toString();
+            } catch (IOException e) {
                 e.printStackTrace();
+                Log.e(TAG, "doInBackground: " + e.getMessage());
             }
 
-            return null;
+            return response;
         }
 
         @Override
-        protected void onPostExecute(String result){
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPostExecute: result = " + result);
             TextView textView = (TextView) findViewById(R.id.textView1);
             textView.setText(result);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        //  getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    int getData(){
-        return 0;
     }
 }
